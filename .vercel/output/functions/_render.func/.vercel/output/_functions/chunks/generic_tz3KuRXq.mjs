6974f029ec4c22e75a56ae994ef5385a62190796
ThomasGysemans <gysemansthomas@gmail.com>
2024-05-01 10:54:54 +0200

    Remove useless files from git repo

--- .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/generic_tz3KuRXq.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/generic_B8dc-3A0.mjs');
-
-export { page };
--- .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/index_CUr27oUY.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./prerender_C4Jr7XfH.mjs').then(n => n.i);
-
-export { page };
